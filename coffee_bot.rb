0bbb0690aa9e115dcae84fcb5b7aa4a40ebcab58
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-17 23:56:46 +0100

    Ajout du README

--- coffee_bot.rb
+++ coffee_bot.rb
@@ -8,4 +8,7 @@ client = Twitter::Streaming::Client.new(config)
 # binding.pry
 p client
 
-tweets = client.user_timeline('Bboylegros', count: 20)
+topics = ["coffee", "tea"]
+client.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
--- twitter_bot.rb
+++ twitter_bot.rb
@@ -1,7 +1,12 @@
+# Appel des différentes gem utilisées
+
 require 'twitter'
 require 'pry'
 require 'inifile'
 
+
+# def de la méthode follow prenant en compte un nombre non défini d'abo
+# follow les users passés en paramètre
 
 def follow(*usrs)
 config = IniFile.load('app.ini')["config"]
@@ -10,10 +15,11 @@ p client
 
 client.follow(usrs.each { |user| user})
 end
-
+# Appel de la fonction follow : décommenter
 # follow("Michbidbup", "Bboylegros")
 
 
+# def de la méthode tweet
 
 def tweet1(tweet)
   config = IniFile.load('app.ini')["config"]
@@ -23,7 +29,7 @@ end
 
 # tweet1("Tweet test 2 @Michbidbup")
 
-
+# def de la fonction tweet qui génère des tweets 
 
 def tweet2(tweet, *usrs)
     config = IniFile.load('app.ini')["config"]
@@ -33,4 +39,5 @@ def tweet2(tweet, *usrs)
     end
 end
 
+# Appel de la fonction tweet
 tweet2("ca tweet sec", "Michbidbup", "WhoisMarvin42", "Bboylegros" )
